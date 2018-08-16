a58fe3e225460be2fd38efaadbb16fc8bb4121a7
tomas9807 <tomas9807@protonmail.com>
2018-08-16 13:55:11 -0400

    xd

---
+++ metadata/manage.py
@@ -0,0 +1,32 @@
+import json
+
+print(json.load('keys.json'))
+
+
+# class LoadJson():
+#     def __init__(self):
+#         data = json.dumps('keys.json')
+
+        
+    
+
+
+# PATTERNS_DEFAULT = {
+    
+#     SOCIOS:{NAME:2,ID:4,ACC:(6,7)},
+#     EMPLEADOS:{ID:3},
+#     OBREROS:{ID:1},
+#     MOVIMIENTOS:{ID:1,APORTE_DEDUC:3},
+
+# }
+
+
+
+
+
+# def get_filename_handfuls(key):
+#     return FILENAMES_HANDFULS.get(key)
+
+
+# def get_default_patterns(key):
+#     return PATTERNS_DEFAULT.get(key)
